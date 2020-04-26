382be68025b94a93e490ad6fc8c899211536ea0a
Harry Ross <hazross@hotmail.com>
2020-04-26 12:31:12 +1000

    restored app

---
+++ app.js
@@ -0,0 +1,36 @@
+// Initialise environment variables
+require('dotenv').config()
+
+// Define express app from config file
+const app = require('./config/express-app')
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const routes = require('./routes/index');
+app.use('/', routes);
+
+const mysql = require('mysql');
+
+const db = mysql.createConnection ({
+    host: process.env.MYSQL_URL,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PWD,
+    database: process.env.MYSQL_DB
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+
+global.db = db;
+
+// Set port
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log(`Running on port ${port}`);
+})
